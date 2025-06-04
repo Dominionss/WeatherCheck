6b4a181d37f7e85246622bd80536f451a85e2170
Dominionss <qshieldt@gmail.com>
2025-06-04 19:51:19 +0300

    Random numbers in cards.

--- weather-app/src/Card.js
+++ weather-app/src/Card.js
@@ -1,10 +1,18 @@
 import "./Card.css";
 
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
 function Card({title, info}) {
     return (
         <div className="Card">
             <h2>{ title }</h2>
             <h3>Info: { info }</h3>
+            <h3>{getRandomIntInclusive(1, 100)}</h3>
         </div>
     );
 }
